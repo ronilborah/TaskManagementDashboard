8a2fdaaeac7088668047ca4dfd80d1cbe4cc8259
ronilborah <ronilborah@gmail.com>
2025-07-25 02:39:26 +0530

    Update index.js and add index.css

--- client/src/index.js
+++ client/src/index.js
@@ -1,31 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import './index.css';
 import App from './App';
 
 import 'react-toastify/dist/ReactToastify.css';
-
-// Inject global styles from index.css
-const globalStyles = `
-body {
-  margin: 0;
-  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
-    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
-    sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-}
-
-code {
-  font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
-    monospace;
-}
-`;
-
-const styleSheet = document.createElement("style");
-styleSheet.type = "text/css";
-styleSheet.innerText = globalStyles;
-document.head.appendChild(styleSheet);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
