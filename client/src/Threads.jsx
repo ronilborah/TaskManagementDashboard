cd13ea57bf6e4ae2f9b3e639b51d9a46bda78fff
ronilborah <ronilborah@gmail.com>
2025-07-25 03:33:13 +0530

    Cleanup: Remove unused manifest.json and robots.txt files

--- client/src/Threads.jsx
+++ client/src/Threads.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useRef } from "react";
 import { Renderer, Program, Mesh, Triangle, Color } from "ogl";
-import "./Threads.css";
+
 
 const vertexShader = `
 attribute vec2 position;
